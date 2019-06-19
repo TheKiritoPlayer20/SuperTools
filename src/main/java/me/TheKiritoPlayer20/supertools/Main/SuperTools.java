f62200d21f60568667d9b5e8f8ed1ad3eada047e
KiritoGames20 <34043946+KiritoGames20@users.noreply.github.com>
2019-06-19 19:37:07 +0200

    SuperTools
    Removed Old SuperTools and Renamed the Example mod aswell as renaming the directorie path.

--- src/main/java/me/TheKiritoPlayer20/supertools/Main/SuperTools.java
+++ src/main/java/me/TheKiritoPlayer20/supertools/Main/SuperTools.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package me.TheKiritoPlayer20.supertools.Main;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,13 +19,13 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("supertools")
+public class SuperTools
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public SuperTools() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -54,7 +54,7 @@ public class ExampleMod
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+        InterModComms.sendTo("supertools", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
