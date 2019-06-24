a997439d6e2ece424e40e19065951627a880eb96
KiritoGames20 <34043946+KiritoGames20@users.noreply.github.com>
2019-06-25 00:28:13 +0200

    Recipes and Armor
    Updated Bone meal tool recipe and reduced quartz armor points

--- src/main/java/me/KG20/supertools/Armor/BasisArmorMaterial.java
+++ src/main/java/me/KG20/supertools/Armor/BasisArmorMaterial.java
@@ -18,7 +18,7 @@ public class BasisArmorMaterial {
 
     public final static IArmorMaterial emerald = new ArmorMaterial(Constants.modid + ":emerald",33, new int[]{4, 5, 8, 3}, 12, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F,() -> Ingredient.fromItems(Items.EMERALD));
     public final static IArmorMaterial obsidian = new ArmorMaterial(Constants.modid + ":obsidian", 33, new int[]{2, 6, 10, 2}, 12, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F,() -> Ingredient.fromItems(Blocks.OBSIDIAN));
-    public final static IArmorMaterial quartz = new ArmorMaterial(Constants.modid + ":quartz", 33, new int[]{3, 5, 8, 4}, 12, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F,() -> Ingredient.fromItems(Items.QUARTZ));
+    public final static IArmorMaterial quartz = new ArmorMaterial(Constants.modid + ":quartz", 33, new int[]{2, 5, 6, 2}, 12, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F,() -> Ingredient.fromItems(Items.QUARTZ));
     public final static IArmorMaterial lapis = new ArmorMaterial(Constants.modid + ":lapis",10, new int[]{2, 3, 4, 2}, 15, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F,() -> Ingredient.fromItems(Items.LAPIS_LAZULI));
 
     private static class ArmorMaterial implements IArmorMaterial{
