3d2fb7f35af057abefd968c5056019b0acc85c30
realKG20 <34043946+realKG20@users.noreply.github.com>
2022-07-11 17:57:12 +0200

    Added OwnBlockTags & cup.json to git

---
+++ src/main/java/me/KG20/supertools/Main/OwnBlockTags.java
@@ -0,0 +1,12 @@
+package me.KG20.supertools.Main;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+
+public class OwnBlockTags {
+
+    public static final TagKey<Block> MINEABLE_WITH_CUP = BlockTags.create(new ResourceLocation("minable/cup"));
+
+}
