e0785a60f9b4e32b269f9c513911c7b51953d576
realKG20 <34043946+realKG20@users.noreply.github.com>
2023-08-20 20:50:43 +0200

    Fixed Repair Item of the Super Tools

--- src/main/java/me/KG20/supertools/Tools/BasisToolMaterial.java
+++ src/main/java/me/KG20/supertools/Tools/BasisToolMaterial.java
@@ -1,5 +1,6 @@
 package me.KG20.supertools.Tools;
 
+import me.KG20.supertools.Init.RegisterItems;
 import net.minecraft.util.LazyLoadedValue;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Tier;
@@ -17,7 +18,7 @@ public class BasisToolMaterial {
     public static final Tier lapis = new ToolMaterial(1, Config.durability_Lapis.get(), 6.0F, Config.attackdamage_Lapis.get(), 20, () -> Ingredient.of(Items.LAPIS_LAZULI));
     public static final Tier copper = new ToolMaterial(2, Config.durability_Copper.get(), 6.0F, Config.attackdamage_Copper.get(), 10, () -> Ingredient.of(Items.COPPER_INGOT));
     public static final Tier redstone = new ToolMaterial(2, Config.durability_Redstone.get(), 15.0F, Config.attackdamage_Redstone.get(), 20, () -> Ingredient.of(Items.REDSTONE));
-    public static final Tier supertools = new ToolMaterial(4, Config.durability_SuperTools.get(), 10.6F, Config.attackdamage_SuperTools.get(), 16, () -> Ingredient.of(Items.DIAMOND));
+    public static final Tier supertools = new ToolMaterial(4, Config.durability_SuperTools.get(), 10.6F, Config.attackdamage_SuperTools.get(), 16, () -> Ingredient.of(RegisterItems.adarum));
     public static final Tier itemcup = new ToolMaterial(4, Config.durability_Cup.get(), 10.6F, Config.attackdamage_Cup.get(),16, () -> Ingredient.of(Items.DIAMOND));
     public static final Tier specialcup = new ToolMaterial(4, Config.durability_SpecialCup.get(), 12F, Config.attackdamage_SpecialCup.get(), 16, () ->Ingredient.of(Items.DIAMOND));
 
